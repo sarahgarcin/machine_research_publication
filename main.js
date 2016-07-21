e3ebd0071fb8d705067845d87f9b2b35b17f6d4a
sarahgarcin <sarahgarcin@hotmail.com>
2016-07-21 18:42:24 +0200

    new version july

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ module.exports = function(app, io){
 	io.on("connection", function(socket){
 
 		//INDEX
-		socket.on( 'listData', function (data){ onListData( socket); });
+		socket.on( 'listData', function (data){ onListImages( socket); });
 		socket.on('savePDF', createPDF);
 
 		// DODOC part
@@ -27,75 +27,111 @@ module.exports = function(app, io){
 // ------------- F U N C T I O N S -------------------
 
 	// ------------- I N D E X  -------------------
+	function onListImages(socket){
+		fs.readdir( 'content/images', function (err, images) {
+			console.log(images);
+		 	if (err) return console.log( 'Couldn\'t read content dir : ' + err);
+		 	images.forEach(function(file){
+		 		if(file.split('.')[1] == 'jpg'){
+			 		socket.emit('sendImages', file);
+			 		console.log(file);
+			 	}
+	 		});
+	 	});
+	}
+
 	function onListData(socket){
-			fs.readdir( 'content/', function (err, dir) {
-	      if (err) return console.log( 'Couldn\'t read content dir : ' + err);
-		 		dir.forEach(function(folder) {
-		 			if(folder !='.DS_Store'){
-			 			fs.readdir('content/'+folder, function (err, files){
-			 				if (err) return console.log( 'Couldn\'t read content dir : ' + err);
-			 				files.forEach(function(file){
-			 					if(file.split('.')[1] == 'txt'){
-			 						fs.readFile('content/'+folder+'/'+file, 'utf8', function(err, data) {
-									  if (err) return console.log( 'Couldn\'t read content file : ' + err);
-									  // console.log('OK: ' + file);
-									  // console.log(data)
-									  socket.emit('sendText', data);
-									});
-			 					}
-			 					if(file.split('.')[1] == 'jpg'){
-									socket.emit('sendImages', file);
-			 					}
-			 				});
-			 			});
-			 		}
-		 		});
-		 	});
+		fs.readdir( 'content/', function (err, dir) {
+      if (err) return console.log( 'Couldn\'t read content dir : ' + err);
+	 		dir.forEach(function(folder) {
+	 			if(folder !='.DS_Store'){
+		 			fs.readdir('content/'+folder, function (err, files){
+		 				if (err) return console.log( 'Couldn\'t read content dir : ' + err);
+		 				files.forEach(function(file){
+		 					if(file.split('.')[1] == 'txt'){
+		 						fs.readFile('content/'+folder+'/'+file, 'utf8', function(err, data) {
+								  if (err) return console.log( 'Couldn\'t read content file : ' + err);
+								  // console.log('OK: ' + file);
+								  // console.log(data)
+								  socket.emit('sendText', data);
+								});
+		 					}
+		 					if(file.split('.')[1] == 'jpg'){
+								socket.emit('sendImages', file);
+		 					}
+		 				});
+		 			});
+		 		}
+	 		});
+	 	});
 	}
 
 	//------------- PDF -------------------
+		// function createPDF(){
+		// 	var url = 'http://localhost:8080/',
+	 // 				pdf = 'pdf/'+getCurrentDate()+'.pdf';
+	 // 				console.log('create PDF');
+	 // 		phantom.create().then(function(ph) {
+		//     ph.createPage().then(function(page) {
+		//     	// console.log(page);
+		// 	      page.open(url).then(function(status) {
+		// 	       	// console.log(status, page);
+		// 		      setTimeout(function() {
+		// 	          if (status === 'success') {
+		// 	          	console.log(status);
+		// 							console.log('wait...');
+		// 		      		page.render(pdf);
+		// 		      		page.close();
+		// 							ph.exit();
+		// 	      //     	page.property('content').then(function(content){
+		// 							//   console.log("Content: ", content);
+		// 							//   page.close();
+		// 							//   ph.exit();
+		// 							//   fs.writeFile("test.html", content, function(err) {
+		// 							// 	  if(err) {
+		// 							// 	    return console.log(err);
+		// 							// 	    page.close();
+		// 							//   		ph.exit();
+		// 							// 	  }
+		// 							// 	  console.log("The file was saved!");
+		// 							// 	  page.close();
+		// 							//   	ph.exit();
+		// 							// 	}); 
+		// 							// 	// wkhtmltopdf(content, {output: 'out.pdf'});
+
+		// 							// });
+		// 	          } 
+		// 	          else {
+		// 	            console.log('some error');
+		// 	           	page.close();
+		// 	            ph.exit();
+		// 	          }
+		// 	        }, 3000);
+		//       	});
+		//     });
+		// 	});
+		// }
+	
 	function createPDF(){
+		var _ph, _page, _outObj;
 		var url = 'http://localhost:8080/',
  				pdf = 'pdf/'+getCurrentDate()+'.pdf';
  				console.log('create PDF');
- 		phantom.create().then(function(ph) {
-	    ph.createPage().then(function(page) {
-	    	// console.log(page);
-		      page.open(url).then(function(status) {
-		       	// console.log(status, page);
-			      setTimeout(function() {
-		          if (status === 'success') {
-		          	console.log(status);
-								console.log('wait...');
-			      		page.render(pdf);
-			      		page.close();
-								ph.exit();
-		      //     	page.property('content').then(function(content){
-								//   console.log("Content: ", content);
-								//   page.close();
-								//   ph.exit();
-								//   fs.writeFile("test.html", content, function(err) {
-								// 	  if(err) {
-								// 	    return console.log(err);
-								// 	    page.close();
-								//   		ph.exit();
-								// 	  }
-								// 	  console.log("The file was saved!");
-								// 	  page.close();
-								//   	ph.exit();
-								// 	}); 
-								// 	// wkhtmltopdf(content, {output: 'out.pdf'});
-
-								// });
-		          } 
-		          else {
-		            console.log('some error');
-		           	page.close();
-		            ph.exit();
-		          }
-		        }, 3000);
-	      	});
-	    });
+		phantom.create().then(ph => {
+	    _ph = ph;
+	    return _ph.createPage();
+		}).then(page => {
+	    _page = page;
+	    return _page.open(url);
+		}).then(status => {
+	    // console.log(status);
+	    return _page.property('content');
+		}).then(content => {
+	    // console.log(content);
+	    _page.property('paperSize', {format: 'A4', orientation: 'landscape', margin: '0.5cm'});
+	    _page.render(pdf);
+	    _page.close();
+	    _ph.exit();
 		});
 	}
 
--- public/js/index.js
+++ public/js/index.js
@@ -28,163 +28,172 @@ jQuery(document).ready(function($) {
 
 
 function init(){
-	var scaleCount = 1;
-	var imagePosY = 0;
-	var imagePosX = 0;
+	
 	setTimeout(function(){
-		// console.log(dataArray);
+		var randomIndex = Math.floor(Math.random() * dataArray.length);
+		var randomFile = dataArray[randomIndex];
+		
+	}, 3000);
 
-		for(var i =0; i<9; i++){
-			var randomIndex = Math.floor(Math.random() * dataArray.length);
-			var randomFile = dataArray[randomIndex];
-			var $randomCell = $('.cell').eq(i);
-			// console.log(randomFile);
-			var ext = randomFile.split('.')[1];
-			$randomCell.attr('data-index', randomIndex);
-			if(ext == 'jpg'){
-				$randomCell.attr('data-ext', 'jpg');
-				$randomCell.html('<img src="images/'+randomFile+'">');
-			}
-			else{
-				$randomCell.attr('data-ext', 'txt');
-				$randomCell.html('<p>'+randomFile+'</p>');
-			}
-		}
-	},3000);
 
-	$(document).on('keypress',function(e){
-		var code = e.keyCode;
-		console.log(code);
-		var $firstCell = $('.cell').eq(0);
-		// press space (32) -> transform the page in pdf
-		if(e.keyCode == 32){
-			socket.emit('savePDF');
-		}
-		//a 97 et e 101
-		// change image in the first cell
-		if(e.keyCode == 97){
-			var dataIndex = $('.cell').eq(0).attr('data-index');
-			$('.cell').eq(0).attr('data-index', (parseInt(dataIndex-1)));
-			var prevFile = dataArray[parseInt((dataIndex-1))];
-			var extension = $('.cell').eq(0).attr('data-ext');
-			console.log(extension);
-			if(extension == 'jpg'){
-				$('.cell').eq(0).html('<img src="images/'+prevFile+'">');
-			}
-			else{
-				$('.cell').eq(0).html('<p>'+prevFile+'</p>');
-			}
-		}
-		if(e.keyCode == 101){
-			var dataIndex = $('.cell').eq(0).attr('data-index');
-			$('.cell').eq(0).attr('data-index', (parseInt(dataIndex)+1));
-			// console.log((parseInt(dataIndex)+1));
-			var nextFile = dataArray[(parseInt(dataIndex)+1)];
-			var extension = $('.cell').eq(0).attr('data-ext');
-			if(extension == 'jpg'){
-				$('.cell').eq(0).html('<img src="images/'+nextFile+'">');
-			}
-			else{
-				$('.cell').eq(0).html('<p>'+nextFile+'</p>');
-			}
-		}
-		// press Z and if zoom in the cell
-		if(e.keyCode == 122){
-			console.log(scaleCount);
-			if(scaleCount > 4){
-				console.log('too big');
-				scaleCount = 1;
-			}
-			else{
-				console.log('increase');
-				scaleCount += 0.2;
-				$firstCell.children().css({
-					'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
-		// press - and if zoom out the cell
-		if(e.keyCode == 45){
-			console.log(scaleCount);
-			if(scaleCount < 0.4){
-				console.log('too small');
-				scaleCount = 1.2;
-			}
-			else{
-				console.log('discrease');
-				scaleCount -= 0.2;
-				$firstCell.children().css({
-					'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
-		// move the image in the cell
-		// press o to move the image up
-		if(e.keyCode == 111){
-			console.log(imagePosY, $firstCell.children().height());
-			if(imagePosY < -$firstCell.children().height()){
-				console.log('too high');
-				imagePosY = 0;
-			}
-			else{
-				console.log('go high');
-				imagePosY -= 10;
-				$firstCell.children().css({
-					'transform': 'translateY('+imagePosY+'px)',
-					// 'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
-		// press l to move the image down
-		if(e.keyCode == 108){
-			console.log(imagePosY, $firstCell.children().height());
-			if(imagePosY > $firstCell.children().height()){
-				console.log('too low');
-				imagePosY = 0;
-			}
-			else{
-				console.log('go low');
-				imagePosY += 10;
-				$firstCell.children().css({
-					'transform': 'translateY('+imagePosY+'px)',
-					// 'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
-		// press k to move the image left
-		if(e.keyCode == 107){
-			console.log(imagePosX, $firstCell.children().width());
-			if(imagePosX < -$firstCell.children().width()){
-				console.log('too low');
-				imagePosX = 0;
-			}
-			else{
-				console.log('go low');
-				imagePosX -= 10;
-				$firstCell.children().css({
-					'transform': 'translateX('+imagePosX+'px)',
-					// 'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
-		// press m to move the image right
-		if(e.keyCode == 109){
-			console.log(imagePosX, $firstCell.children().width());
-			if(imagePosX > $firstCell.children().width()){
-				console.log('too high');
-				imagePosX = 0;
-			}
-			else{
-				console.log('go high');
-				imagePosX += 10;
-				$firstCell.children().css({
-					'transform': 'translateX('+imagePosX+'px)',
-					// 'transform': 'scale('+scaleCount+')'
-				});
-			}
-		}
+	// var scaleCount = 1;
+	// var imagePosY = 0;
+	// var imagePosX = 0;
+	// setTimeout(function(){
+	// 	// console.log(dataArray);
 
-	});	
+	// 	for(var i =0; i<9; i++){
+	// 		var randomIndex = Math.floor(Math.random() * dataArray.length);
+	// 		var randomFile = dataArray[randomIndex];
+	// 		var $randomCell = $('.cell').eq(i);
+	// 		// console.log(randomFile);
+	// 		var ext = randomFile.split('.')[1];
+	// 		$randomCell.attr('data-index', randomIndex);
+	// 		if(ext == 'jpg'){
+	// 			$randomCell.attr('data-ext', 'jpg');
+	// 			$randomCell.html('<img src="images/'+randomFile+'">');
+	// 		}
+	// 		else{
+	// 			$randomCell.attr('data-ext', 'txt');
+	// 			$randomCell.html('<p>'+randomFile+'</p>');
+	// 		}
+	// 	}
+	// },3000);
+
+	// Action on keypress
+		// $(document).on('keypress',function(e){
+		// 	var code = e.keyCode;
+		// 	console.log(code);
+		// 	var $firstCell = $('.cell').eq(0);
+		// 	// press space (32) -> transform the page in pdf
+		// 	if(e.keyCode == 32){
+		// 		socket.emit('savePDF');
+		// 	}
+		// 	//a 97 et e 101
+		// 	// change image in the first cell
+		// 	if(e.keyCode == 97){
+		// 		var dataIndex = $('.cell').eq(0).attr('data-index');
+		// 		$('.cell').eq(0).attr('data-index', (parseInt(dataIndex-1)));
+		// 		var prevFile = dataArray[parseInt((dataIndex-1))];
+		// 		var extension = $('.cell').eq(0).attr('data-ext');
+		// 		console.log(extension);
+		// 		if(extension == 'jpg'){
+		// 			$('.cell').eq(0).html('<img src="images/'+prevFile+'">');
+		// 		}
+		// 		else{
+		// 			$('.cell').eq(0).html('<p>'+prevFile+'</p>');
+		// 		}
+		// 	}
+		// 	if(e.keyCode == 101){
+		// 		var dataIndex = $('.cell').eq(0).attr('data-index');
+		// 		$('.cell').eq(0).attr('data-index', (parseInt(dataIndex)+1));
+		// 		// console.log((parseInt(dataIndex)+1));
+		// 		var nextFile = dataArray[(parseInt(dataIndex)+1)];
+		// 		var extension = $('.cell').eq(0).attr('data-ext');
+		// 		if(extension == 'jpg'){
+		// 			$('.cell').eq(0).html('<img src="images/'+nextFile+'">');
+		// 		}
+		// 		else{
+		// 			$('.cell').eq(0).html('<p>'+nextFile+'</p>');
+		// 		}
+		// 	}
+		// 	// press Z and if zoom in the cell
+		// 	if(e.keyCode == 122){
+		// 		console.log(scaleCount);
+		// 		if(scaleCount > 4){
+		// 			console.log('too big');
+		// 			scaleCount = 1;
+		// 		}
+		// 		else{
+		// 			console.log('increase');
+		// 			scaleCount += 0.2;
+		// 			$firstCell.children().css({
+		// 				'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+		// 	// press - and if zoom out the cell
+		// 	if(e.keyCode == 45){
+		// 		console.log(scaleCount);
+		// 		if(scaleCount < 0.4){
+		// 			console.log('too small');
+		// 			scaleCount = 1.2;
+		// 		}
+		// 		else{
+		// 			console.log('discrease');
+		// 			scaleCount -= 0.2;
+		// 			$firstCell.children().css({
+		// 				'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+		// 	// move the image in the cell
+		// 	// press o to move the image up
+		// 	if(e.keyCode == 111){
+		// 		console.log(imagePosY, $firstCell.children().height());
+		// 		if(imagePosY < -$firstCell.children().height()){
+		// 			console.log('too high');
+		// 			imagePosY = 0;
+		// 		}
+		// 		else{
+		// 			console.log('go high');
+		// 			imagePosY -= 10;
+		// 			$firstCell.children().css({
+		// 				'transform': 'translateY('+imagePosY+'px)',
+		// 				// 'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+		// 	// press l to move the image down
+		// 	if(e.keyCode == 108){
+		// 		console.log(imagePosY, $firstCell.children().height());
+		// 		if(imagePosY > $firstCell.children().height()){
+		// 			console.log('too low');
+		// 			imagePosY = 0;
+		// 		}
+		// 		else{
+		// 			console.log('go low');
+		// 			imagePosY += 10;
+		// 			$firstCell.children().css({
+		// 				'transform': 'translateY('+imagePosY+'px)',
+		// 				// 'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+		// 	// press k to move the image left
+		// 	if(e.keyCode == 107){
+		// 		console.log(imagePosX, $firstCell.children().width());
+		// 		if(imagePosX < -$firstCell.children().width()){
+		// 			console.log('too low');
+		// 			imagePosX = 0;
+		// 		}
+		// 		else{
+		// 			console.log('go low');
+		// 			imagePosX -= 10;
+		// 			$firstCell.children().css({
+		// 				'transform': 'translateX('+imagePosX+'px)',
+		// 				// 'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+		// 	// press m to move the image right
+		// 	if(e.keyCode == 109){
+		// 		console.log(imagePosX, $firstCell.children().width());
+		// 		if(imagePosX > $firstCell.children().width()){
+		// 			console.log('too high');
+		// 			imagePosX = 0;
+		// 		}
+		// 		else{
+		// 			console.log('go high');
+		// 			imagePosX += 10;
+		// 			$firstCell.children().css({
+		// 				'transform': 'translateX('+imagePosX+'px)',
+		// 				// 'transform': 'scale('+scaleCount+')'
+		// 			});
+		// 		}
+		// 	}
+
+		// });	
 
 }
 
